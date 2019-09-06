ecc17583a6c2fbbbd620d82cbdc10825db903e2b
Ankit kumar <ankkumar11@publicisgroupe.net>
2019-09-06 14:57:53 +0530

    commit

---
+++ trading-micro-service/src/main/java/com/sapient/microservices/tradingmicroservice/TradingMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.microservices.tradingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingMicroServiceApplication.class, args);
+	}
+
+}
